cb28a0dde9fdfe721f499401443c7086e8f04c42
Evgeniy Schibrikov <shook@live.ru>
2017-07-09 22:04:11 +0500

    Global changes

--- src/components/table/__seat/seat.js
+++ src/components/table/__seat/seat.js
@@ -1,20 +1,26 @@
 import React, { Component } from 'react';
 import './seat.css';
-// import card_example from './ace_of_spades.png';
-import card_example from './QC.svg';
+
+const Card = (props) => (
+  <img className="pl-seat-card"
+    src={`${process.env.PUBLIC_URL}/cards-svg/${props.data.type}${props.data.suit}.svg`}
+    alt="Left card" />
+)
 
 class PlayerSeat extends Component {
   render() {
     return (
-      <div className="pl-seat-wrapper">
+      <div className="table-seat pl-seat-wrapper">
         <div className="pl-seat-cards">
-          <img className="pl-seat-card pl-seat-card-left" src={card_example} alt="Left card" />
-          <img className="pl-seat-card pl-seat-card-right" src={card_example} alt="Right card" />
+          <Card data={this.props.info.hand.first} />
+          <Card data={this.props.info.hand.second} />
+          {/* <img className="pl-seat-card pl-seat-card-left" src={card_example} alt="Left card" />
+          <img className="pl-seat-card pl-seat-card-right" src={card_example} alt="Right card" /> */}
         </div>
         <div className="pl-seat-panel">
-          <h1 className="pl-seat-nickname">sHooK</h1>
+          <h1 className="pl-seat-nickname">{this.props.info.nickname}</h1>
           <hr className="pl-seat-splitter"/>
-          <h2 className="pl-seat-bank">1783$</h2>
+          <h2 className="pl-seat-bank">{this.props.info.bank}$</h2>
         </div>
       </div>
     );
@@ -22,3 +28,12 @@ class PlayerSeat extends Component {
 }
 
 export default PlayerSeat;
+
+/*
+  PlayerInfo {
+    nickname,
+    avatar,
+    bank,
+    hand
+}
+*/
--- src/components/table/table.js
+++ src/components/table/table.js
@@ -2,13 +2,33 @@ import React, {Component} from 'react';
 import './table.css';
 import PlayerSeat from './__seat/seat';
 
+const example = {
+  nickname: 'shook',
+  bank: '3459',
+  hand: {
+    first: {
+      type: 'Q',
+      suit: 'C'
+    },
+    second: {
+      type: '10',
+      suit: 'D'
+    }
+  }
+}
+
 class Table extends Component {
     render() {
         return (
           <div className="table-wrapper">
-            <PlayerSeat/>
-            {/* <PlayerSeat/>
-            <PlayerSeat/> */}
+            <div className="table-background">
+              <PlayerSeat info={example} />
+              <PlayerSeat info={example} />
+              <PlayerSeat info={example} />
+              <PlayerSeat info={example} />
+              <PlayerSeat info={example} />
+              <PlayerSeat info={example} />
+            </div>
           </div>
         )
     }
