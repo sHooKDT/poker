4bf4c6bab089218c046cb9a546b7b64a4386228c
Evgeniy <shook@live.ru>
2018-09-19 11:52:02 +0500

    removed landing page

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,7 @@
-import React, { Component } from 'react';
-// import io from 'socket.io-client';
-// import Table from './Table';
-import Landing from './Landing/';
+import React from 'react';
+import Table from './Table';
 
-class App extends Component {
-  componentWillMount() {
-    // const socket = io('http://localhost:4000', {
-    //   path: '/test',
-    // });
-    // socket.connect();
-    // socket.send('Hello, motherfuckers!');
-    // socket.on('message', (msg) => {
-    //   window.alert(msg);
-    // });
-  }
 
-  render() {
-    return <Landing />;
-    // return <Table />;
-  }
+export default function App() {
+  return <Table />;
 }
-
-export default App;
--- src/components/Landing/SignUpFrame/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import FormFrame from '../FormFrame';
-
-export default function SignUpFrame() {
-  return <FormFrame />;
-}
